1b58e1ada41c09c6fdec28cb712fa4ba8f98dacc
paulwsibley@gmail.com <105944002+ps1b@users.noreply.github.com>
2022-06-09 17:40:33 -0400

    Update P3-Install.sh

--- scripts/P3-Install.sh
+++ scripts/P3-Install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/sh
 
-echo -e "### Downloading and installing files...\n###"
+echo -e "### Creating zeek user and group"
+groupadd zeek && useradd zeek -g zeek
+echo -e "### Creating create /opt/zeek and assign permissions"
+mkdir /opt/zeek /home/zeek/.ssh
+chown -R zeek:zeek /opt/zeek/ /home/zeek
+echo -e "### Creating RSA key pair"
+ssh-keygen -q -t rsa -b 4096 -N '' -f /home/zeek/.ssh/id_rsa <<<y >/dev/null 2>&1
 
+echo -e "### Fixing permissions for zeek user..."
+chown -R zeek:zeek /opt/zeek/ /home/zeek
+
+echo -e "### Downloading and installing files...\n###"
 echo -e "### installing promisc.service"
 wget https://raw.githubusercontent.com/ps1b/canids/main/scripts/promisc.service
 chmod u+x promisc.service
@@ -15,16 +25,16 @@ chmod u+x zeek.service
 mv zeek.service /etc/systemd/system/
 systemctl enable zeek.service
 
+echo -e "### Installing crontab items"
+
+
 echo -e "### Fetching rsync.sh"
 wget https://raw.githubusercontent.com/ps1b/canids/main/scripts/rsync.sh
 chmod +x rsync.sh
-
-echo -e "### Creating RSA key pair"
-ssh-keygen -q -t rsa -b 4096 -N '' -f /home/zeek/.ssh/id_rsa <<<y >/dev/null 2>&1
+mv rsync.sh /home/zeek
 
 echo -e "### Fixing permissions for zeek user..."
-chown -R zeek:zeek /home/zeek
-
+chown -R zeek:zeek /opt/zeek/ /home/zeek
 
 echo -e "### Create Summary file collection.txt ...\n###"
 echo -e "### Summary" >> collection.txt
@@ -42,5 +52,5 @@ echo -e "### rsa.pub\n###" >> collection.txt
 cat /home/zeek/.ssh/id_rsa.pub >> collection.txt
 echo -e "\n###" >> collection.txt
 
-echo ### Done
+echo -e "### Done"
 Done
