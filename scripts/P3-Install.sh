2dbaa4f9e3196d6f3ac7fb0195e1a82dc05f6d79
paulwsibley@gmail.com <105944002+ps1b@users.noreply.github.com>
2022-06-09 18:09:01 -0400

    Update P3-Install.sh

--- scripts/P3-Install.sh
+++ scripts/P3-Install.sh
@@ -3,18 +3,26 @@
 echo -e "Enabling Powertools"
 sed -i -e "s|enabled=0|enabled=1|g" /etc/yum.repos.d/CentOS-Stream-PowerTools.repo
 
+echo -e "Install zeek repository"
+cd /etc/yum.repos.d/ && wget https://download.opensuse.org/repositories/security:zeek/CentOS_8_Stream/security:zeek.repo
+
+echo -e "Install Dependencies"
+yum -y install cmake make gcc gcc-c++ flex bison libpcap-devel openssl-devel platform-python-devel swig zlib-devel kernel-devel kernel-headers python36 network-scripts tar wget gdb git python3-pip sendmail sendmail-cf rsync unzip epel-release tcpdump zeek-lts zeek-lts-devel
+
 echo -e "### Creating zeek user and group"
 groupadd zeek && useradd zeek -g zeek
 echo -e "### Creating create /opt/zeek and assign permissions"
-mkdir /opt/zeek /home/zeek/.ssh
+mkdir /opt/zeek /home/zeek/.ssh /home/zeek/zeek-install
 chown -R zeek:zeek /opt/zeek/ /home/zeek
 echo -e "### Creating RSA key pair"
 ssh-keygen -q -t rsa -b 4096 -N '' -f /home/zeek/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 echo -e "### Fixing permissions for zeek user..."
-chown -R zeek:zeek /opt/zeek/ /home/zeek
+chown -R zeek:zeek /opt/zeek/ /home/zeek && setcap cap_net_raw=eip /opt/zeek/bin/zeek && setcap cap_net_raw=eip /opt/zeek/bin/capstats
 
-echo -e "### Downloading and installing files...\n###"
+
+echo -e "### Downloading and installing files into /home/zeek/install\n###"
+cd /home/zeek/zeek-install
 echo -e "### installing promisc.service"
 wget https://raw.githubusercontent.com/ps1b/canids/main/scripts/promisc.service
 chmod u+x promisc.service
