33375bbeb1e96afb1207bb96e7abb9e897b30629
paulwsibley@gmail.com <105944002+ps1b@users.noreply.github.com>
2022-06-09 16:19:27 -0400

    Update P3-Install.sh

--- scripts/P3-Install.sh
+++ scripts/P3-Install.sh
@@ -17,6 +17,34 @@ chmod u+x zeek.service
 mv zeek.service /etc/systemd/system/
 systemctl enable zeek.service
 
+echo ### Fetching rsync.sh
+wget https://raw.githubusercontent.com/ps1b/canids/main/scripts/rsync.sh
+chmod +x rsync.sh
+
+echo ### Creating RSA key pair
+ssh-keygen -q -t rsa -b 4096 -N '' -f /home/zeek/.ssh/id_rsa <<<y >/dev/null 2>&1
+
+# Fix permissions for zeek user
+chown -R zeek:zeek /home/zeek
+
+
+echo ###
+echo ### Create Summary file 'collection.txt'...
+echo ###
+echo ### IDS Gateway:
+curl ifconfig.me >> collection.txt
+echo ### rsa.pub >> collection.txt
+cat /home/zeek/.ssh/id_rsa.pub >> collection.txt
+echo ###
+
+
+echo ### Fetching rsync.sh
+wget https://raw.githubusercontent.com/ps1b/canids/main/scripts/rsync.sh
+chmod +x rsync.sh
+
+# Fix permissions for zeek user
+chown -R zeek:zeek /home/zeek
+
 echo 
 
 
